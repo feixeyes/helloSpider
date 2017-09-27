e5c79fc31cb610b29dc55b3b4b89c05b8a2d3971
郭鹏飞 <guopengfei@cloudin.kmail.com>
2017-09-27 21:11:29 +0800

    inital project

---
+++ helloSpider/spiders/qqtv.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from helloSpider.items import HellospiderItem
+
+# scrapy crawl qqtv
+
+class QqtvSpider(CrawlSpider):
+    name = 'qqtv'
+    allowed_domains = ['v.qq.com' ]
+    start_urls = ['http://v.qq.com/x/list/tv']
+
+
+
+    # body > div.site_container.container_main > div > div > div.mod_pages > a.page_next
+
+    rules = (
+        Rule(LinkExtractor(allow=(), restrict_css=('.page_next',)),
+             callback="parse_item",
+             follow=True),)
+
+    def parse_item(self, response):
+        print('Processing..' + response.url)
+        item_links = response.css('.figure_title > a::attr(href)').extract()
+
+        for a in item_links:
+            yield scrapy.Request(a, callback=self.parse_detail_page)
+
+    def parse_detail_page(self, response):
+        print('Processing detail..' + response.url)
+        # container_player > div > div.player_headline.player_headline_mood.cf > div.mod_intro > div.video_base._base > h1 > a
+        # leftdown_content > div:nth-child(1) > div.mod_bd > ul > li.mod_summary.intro_item > div.director
+        # leftdown_content > div:nth-child(1) > div.mod_bd > ul > li.mod_summary.intro_item > div.video_summary.open > p
+
+
+        title = response.css('h1::text').extract()[0].strip()
+        director = response.css('.pricelabel > strong::text').extract()[0]
+        summary = response.css('.pricelabel > strong::text').extract()[0]
+
+        item = HellospiderItem()
+        item['title'] = title
+        item['price'] = director
+        item['summary'] = summary
+        item['url'] = response.url
+        yield item
+
+    # def parse(self, response):
+    #     pass
+
+
+        #detail https://v.qq.com/x/cover/jzhtr2cgy35ejz0.html?
+# <strong class="figure_title"><a href="https://v.qq.com/x/cover/lrwweimk8hanlk8.html" target="_blank" title="我的奇妙男友" _stat2="videos:title">我的奇妙男友</a></strong>
